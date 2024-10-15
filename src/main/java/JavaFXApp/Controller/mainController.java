8d8e7f104f5b0e7bc7fee5bd6a75cf77188591f5
makar.peter <makar.peter98@gmail.com>
2024-10-15 08:29:12 +0200

    Felület létrehozva!

---
+++ src/main/java/JavaFXApp/Controller/mainController.java
@@ -0,0 +1,17 @@
+package JavaFXApp.Controller;
+
+import javafx.event.ActionEvent;
+
+public class mainController {
+    public void onStartDatabaseButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onStopDataBaseButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onAddElekButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void onListElementsButtonClick(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/JavaFXApp/javaFXMain.java
@@ -0,0 +1,4 @@
+package JavaFXApp;
+
+public class javaFXMain {
+}
