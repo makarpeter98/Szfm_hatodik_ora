473a00590f5c78f7b5a65f3d61fdeaa58cbb476c
makar.peter <makar.peter98@gmail.com>
2024-10-29 09:12:21 +0100

    test save person megírva

--- src/test/java/com/example/springjavafx/springApp/SpringTestDemo.java
+++ src/test/java/com/example/springjavafx/springApp/SpringTestDemo.java
@@ -2,8 +2,14 @@ package com.example.springjavafx.springApp;
 
 import com.example.springjavafx.springApp.model.Person;
 import com.example.springjavafx.springApp.model.PersonRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 public class SpringTestDemo {
@@ -20,4 +26,26 @@ public class SpringTestDemo {
         TestAntal.setName("Trab Antal");
         TestAntal.setAge(99);
     }
+
+
+    @Test
+    @Rollback
+    public void testSavePerson()
+    {
+        personRepository.save(TestAntal);
+
+        Optional<Person> person = personRepository.findById(TestAntal.getId());
+
+        assertThat(person).isPresent();
+        assertThat(person.get().getName()).isEqualTo("Trab Antal");
+        assertThat(person.get().getAge()).isEqualTo(99);
+    }
+
+//    @Test
+//    @Rollback
+//
+//    public void testUpdate()
+//    {
+//        personRepository.save(TestAgnes)
+//    }
 }
